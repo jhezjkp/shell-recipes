d8e95d447d2da51f5a18ebd1afe7f741d58fddd2
jhezjkp <jhezjkp@163.com>
2013-01-23 23:12:04 +0800

    文件查看器

---
+++ misc/view.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#==
+#== 文件查看
+#==
+
+#判断参数个数
+if [ $# -lt "1" ]
+then
+    echo "usage: $0 <file_name>"
+    exit 1
+fi
+#行计数器
+counter=1
+cat $1 | while read line
+do
+    echo "Line $counter:\t$line"
+    counter=$[ $counter + 1 ]
+done
+exit 0
