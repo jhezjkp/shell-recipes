88e72e1a95e700f4358d4bb153b66374235f5d3d
jhezjkp <jhezjkp@163.com>
2013-01-24 07:00:08 +0100

    通用随机密码生成方案

--- misc/interactive.sh
+++ misc/interactive.sh
@@ -4,12 +4,19 @@
 #== 交互式脚本：读取用户输入的密码，在5秒内未输入则生成一个
 #==
 
+function generatePassword() {
+    echo `< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8}`
+    return 0
+}
+
+
 if read -t 5 -p "Pls enter a password:" password
 then
     echo "$password"
 else
     echo -e "\ntime out...generating password...done!"
-    passwod=`makepasswd --chars=6`
+    #passwod=`makepasswd --chars=6`
+    passwod=`generatePassword 6`
     echo "$passwod"
 fi
 #显示后退出程序
