6bd09fa736786887137fd7ba21faf531823cb724
jhezjkp <jhezjkp@163.com>
2013-01-24 22:59:13 -0600

    update vim set up script to adapter diffrent distributions

--- basic/vim_script.sh
+++ basic/vim_script.sh
@@ -2,33 +2,35 @@
 
 # Set Up Vim Environment
 
-#check system distribution
-distribution=`lsb_release -is`
+. `pwd`/helper.sh
 
-if test $distribution=="Ubuntu"
+echo "ensure vim is installed and version above 7.0"
+which vim >> /dev/null 2>&1
+if [ $? -eq 1 ]
 then
-    echo "=== Ubuntu =="
-    echo "ensure vim is installed and version above 7.0"
-    #apt-get install vim
-    vimVersion=`vim --version  | sed -n '1p' | cut  -d " " -f5`
-    if [ $(echo "${vimVersion} >= 7" | bc) -eq 1 ]
-    then
-        echo "vim version is ${vimVersion}, pass..."
-        #configure vim
-        echo "configure vim..."
-        cd ~
-        curl -L https://github.com/jhezjkp/dot-vimrc/raw/master/tools/install.sh | sh >> /dev/null
-        cd ~/.vim
-        git checkout vivia
-        echo "\n\ninstall Powerline Python package:"
-        echo "   sudo pip install https://github.com/Lokaltog/powerline/tarball/develop"
-        echo "\nYour vim is now configured!\nPlease open vim and run \"\033[44;37m:BundleInstall\033[0m\" \
-            command to finish the vim plugins installation.\033[0m"]]]
-        exit 0
-    else
-        echo "vim is not installed or version to low(<7.0), please upgrade it before we continue."
-        exit 1
-    fi
+    printf "vim is not installed!"
+exit 1
+fi
+
+vimVersion=`vim --version  | sed -n '1p' | cut  -d " " -f5`
+if [ $(echo "${vimVersion} >= 7" | bc) -eq 1 ]
+then
+    echo "vim version is ${vimVersion}, pass..."
+
+    #configure vim
+    echo "configure vim..."
+    cd ~
+    curl -L https://github.com/jhezjkp/dot-vimrc/raw/master/tools/install.sh | sh >> /dev/null
+    cd ~/.vim
+    git checkout vivia
+    printf "\n\ninstall Powerline Python package:\n\t"
+    print "pip install https://github.com/Lokaltog/powerline/tarball/develop"
+    printf "\nYour vim is now configured!\nPlease open vim and run \n"
+    printf "\t"
+    print ":BundleInstall"
+    echo "command to finish the vim plugins installation."
+    exit 0
 else
-    echo "not ubuntu"
+    echo "vim is not installed or version to low(<7.0), please upgrade it before we continue."
+    exit 1
 fi
