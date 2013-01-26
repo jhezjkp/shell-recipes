0acb63e1bb59cd1d44c560eafb107747dde7f58d
jhezjkp <jhezjkp@163.com>
2013-01-25 22:21:06 -0600

    函数化，方便调用

--- basic/scaffold.sh
+++ basic/scaffold.sh
@@ -36,50 +36,64 @@
 #== 0. replace default update source
 #==
 
-#mirror site url
-mirrorUrl="http://mirrors.163.com/"`echo ${os_name} | tr 'A-Z' 'a-z'`/
-echo $mirrorUrl
+function replace_repository() {
+    #mirror site url
+    mirrorUrl="http://mirrors.163.com/"`echo ${os_name} | tr 'A-Z' 'a-z'`/
+    echo $mirrorUrl
 
-if ! grep -q "mirrors.163.com" /etc/apt/sources.list
-then
-    #get release code name
-    codeName=`echo ${os_code} | tr 'A-Z' 'a-z'`
-    #backup sources.list
-    mv /etc/apt/sources.list /etc/apt/sources.list.bak
-    repoFile="/etc/apt/sources.list"
-    touch ${repoFile}
-    if [ "$os_name" == "Ubuntu" ]
+    if ! grep -q "mirrors.163.com" /etc/apt/sources.list
     then
-	    echo "deb ${mirrorUrl} ${codeName} main restricted universe multiverse" >> ${repoFile}
-	    for var in security updates proposed backports
-	    do
-	        echo "deb ${mirrorUrl} ${codeName}-${var} main restricted universe multiverse" >> ${repoFile}
-	        echo "deb-src ${mirrorUrl} ${codeName}-${var} main restricted universe multiverse" >> ${repoFile}
-	    done
-    elif [ "$os_name" == "Debian" ]
-    then
-		echo "deb ${mirrorUrl} ${codeName} main non-free contrib" >> ${repoFile}
-		echo "deb ${mirrorUrl} ${codeName}-proposed-updates main non-free contrib" >> ${repoFile}
-		echo "deb-src ${mirrorUrl} ${codeName} main non-free contrib" >> ${repoFile}
-		echo "deb-src ${mirrorUrl} ${codeName}-proposed-updates main non-free contrib" >> ${repoFile}
-	else
-exit 1
-  fi
-    #update repository
-    apt-get update
-fi
+        #get release code name
+        codeName=`echo ${os_code} | tr 'A-Z' 'a-z'`
+        #backup sources.list
+        mv /etc/apt/sources.list /etc/apt/sources.list.bak
+        repoFile="/etc/apt/sources.list"
+        touch ${repoFile}
+        if [ "$os_name" == "Ubuntu" ]
+        then
+            echo "deb ${mirrorUrl} ${codeName} main restricted universe multiverse" >> ${repoFile}
+            for var in security updates proposed backports
+            do
+                echo "deb ${mirrorUrl} ${codeName}-${var} main restricted universe multiverse" >> ${repoFile}
+                echo "deb-src ${mirrorUrl} ${codeName}-${var} main restricted universe multiverse" >> ${repoFile}
+            done
+        elif [ "$os_name" == "Debian" ]
+        then
+            echo "deb ${mirrorUrl} ${codeName} main non-free contrib" >> ${repoFile}
+            echo "deb ${mirrorUrl} ${codeName}-proposed-updates main non-free contrib" >> ${repoFile}
+            echo "deb-src ${mirrorUrl} ${codeName} main non-free contrib" >> ${repoFile}
+            echo "deb-src ${mirrorUrl} ${codeName}-proposed-updates main non-free contrib" >> ${repoFile}
+        else
+    exit 1
+      fi
+        #update repository
+        apt-get update
+    fi
+}
+
+
 
 #==
 #== 1. install neccesary packages
 #==
-log "===== install basic software packages ======"
-install_package make wget curl unzip zip gcc git lrzsz python-setuptools \
-    python-pip vim htop iotop nmon makepasswd bc
-log "==== basic software packages installed ====="
+
+function install_basic_packages() {
+    log "===== install basic software packages ======"
+    install_package make wget curl unzip zip gcc git lrzsz python-setuptools \
+        python-pip vim htop iotop nmon makepasswd bc
+    log "==== basic software packages installed ====="
+}
 
 #==
 #== 2. install python packages
 #==
-echo "==== install python packages ===="
-sudo pip install virtualenv virtualenvwrapper
-echo "==== python packages installed ===="
+
+function install_basic_python_packages() {
+    echo "==== install python packages ===="
+    sudo easy_installvirtualenv virtualenvwrapper
+    echo "==== python packages installed ===="
+}
+
+replace_repository
+install_basic_packages
+install_basic_python_packages
