36a0228b9ac678dc2085e9fc7bd48b5c5e692166
jhezjkp <jhezjkp@163.com>
2013-01-26 01:57:41 -0600

    脚本修正

--- basic/zsh_script.sh
+++ basic/zsh_script.sh
@@ -5,17 +5,23 @@
 . `pwd`/helper.sh
 
 echo "check if zsh is installed..."
-if [ `which zsh` ]
+which zsh >> /dev/null 2>&1
+if [ $? -eq 0 ]
 then
     zshVersion=`zsh --version  | sed -n '1p' | cut  -d " " -f2`
     echo "zsh version is ${zshVersion}, pass..."
+    if [ -d ~/.oh-my-zsh ]
+    then
+        echo "zsh has been configured already."
+        exit 0
+    fi
     #configure zsh
     echo "configure zsh..."
     cd ~
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
     exit 0
 else
-    echo "zsh is not installed or version to low(<5.0.0), please upgrade it before we continue"
+    echo "zsh is not installed, please install it before we continue"
     #echo "    \033[44;37msudo apt-get install zsh\033[0m"
     get_install_cmd zsh
     print "\t$return_var"
