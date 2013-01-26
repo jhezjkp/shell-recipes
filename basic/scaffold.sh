2946adc818f2b1e413e7b7f6c2a70b3cd6971379
jhezjkp <jhezjkp@163.com>
2013-01-25 22:29:17 -0600

    更新源替换方法的适配

--- basic/scaffold.sh
+++ basic/scaffold.sh
@@ -37,37 +37,59 @@
 #==
 
 function replace_repository() {
-    #mirror site url
-    mirrorUrl="http://mirrors.163.com/"`echo ${os_name} | tr 'A-Z' 'a-z'`/
-    echo $mirrorUrl
-
-    if ! grep -q "mirrors.163.com" /etc/apt/sources.list
+    if [ "$os_name" = "CentOS" ]
     then
-        #get release code name
-        codeName=`echo ${os_code} | tr 'A-Z' 'a-z'`
-        #backup sources.list
-        mv /etc/apt/sources.list /etc/apt/sources.list.bak
-        repoFile="/etc/apt/sources.list"
-        touch ${repoFile}
-        if [ "$os_name" == "Ubuntu" ]
+        os_version=`cat /etc/issue | sed -n 1p | cut -d ' ' -f3 | cut -c1`
+        #repo
+        repoFile="CentOS${os_version}-Base-163.repo"
+        repoUrl="http://mirrors.163.com/.help/${repoFile}"
+        echo $repoUrl
+
+        #check if the 163 repo file exists
+        if [ ! -f "/etc/yum.repos.d/${repoFile}" ]
         then
-            echo "deb ${mirrorUrl} ${codeName} main restricted universe multiverse" >> ${repoFile}
-            for var in security updates proposed backports
-            do
-                echo "deb ${mirrorUrl} ${codeName}-${var} main restricted universe multiverse" >> ${repoFile}
-                echo "deb-src ${mirrorUrl} ${codeName}-${var} main restricted universe multiverse" >> ${repoFile}
-            done
-        elif [ "$os_name" == "Debian" ]
-        then
-            echo "deb ${mirrorUrl} ${codeName} main non-free contrib" >> ${repoFile}
-            echo "deb ${mirrorUrl} ${codeName}-proposed-updates main non-free contrib" >> ${repoFile}
-            echo "deb-src ${mirrorUrl} ${codeName} main non-free contrib" >> ${repoFile}
-            echo "deb-src ${mirrorUrl} ${codeName}-proposed-updates main non-free contrib" >> ${repoFile}
+            #backup origin repoFile
+            mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+            wget ${repoUrl} -P /etc/yum.repos.d/
+            yum makecache
         else
-    exit 1
-      fi
-        #update repository
-        apt-get update
+            echo "163 repo has bean set!"            
+        fi
+    elif [ "$os_name" = "Ubuntu" ] || [ "$os_name" = "Debian" ]
+    then
+        #mirror site url
+        mirrorUrl="http://mirrors.163.com/"`echo ${os_name} | tr 'A-Z' 'a-z'`/
+        echo $mirrorUrl
+
+        if ! grep -q "mirrors.163.com" /etc/apt/sources.list
+        then
+            #get release code name
+            codeName=`echo ${os_code} | tr 'A-Z' 'a-z'`
+            #backup sources.list
+            mv /etc/apt/sources.list /etc/apt/sources.list.bak
+            repoFile="/etc/apt/sources.list"
+            touch ${repoFile}
+            if [ "$os_name" == "Ubuntu" ]
+            then
+                echo "deb ${mirrorUrl} ${codeName} main restricted universe multiverse" >> ${repoFile}
+                for var in security updates proposed backports
+                do
+                    echo "deb ${mirrorUrl} ${codeName}-${var} main restricted universe multiverse" >> ${repoFile}
+                    echo "deb-src ${mirrorUrl} ${codeName}-${var} main restricted universe multiverse" >> ${repoFile}
+                done
+            elif [ "$os_name" == "Debian" ]
+            then
+                echo "deb ${mirrorUrl} ${codeName} main non-free contrib" >> ${repoFile}
+                echo "deb ${mirrorUrl} ${codeName}-proposed-updates main non-free contrib" >> ${repoFile}
+                echo "deb-src ${mirrorUrl} ${codeName} main non-free contrib" >> ${repoFile}
+                echo "deb-src ${mirrorUrl} ${codeName}-proposed-updates main non-free contrib" >> ${repoFile}
+            else
+        exit 1
+          fi
+            #update repository
+            apt-get update
+        fi
+
     fi
 }
 
