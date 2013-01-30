501257d0b3553e9e92189f86d014a8b22dc2afc4
jhezjkp <jhezjkp@163.com>
2013-01-30 11:29:12 +0100

    兼容centos

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -14,14 +14,19 @@ else
 fi
 
 #install
-echo mysql-server mysql-server/root_password password $password | sudo debconf-set-selections
-echo mysql-server mysql-server/root_password_again password $password | sudo debconf-set-selections
+echo mysql-server mysql-server/root_password password $password | debconf-set-selections
+echo mysql-server mysql-server/root_password_again password $password | debconf-set-selections
 if is_ubuntu
 then
     install_package mysql-client-5.5 mysql-server-5.5 libmysqlclient-dev
 elif is_debian
 then
     install_package mysql-client libmysqlclient-dev mysql-server
+elif is_centos
+then
+    install_package mysql mysql-server
+    service mysqld start
+    /usr/bin/mysqladmin -u root password $password
 fi
 echo -en "\n\nThe MySQL root password is:\t"
 print $password
