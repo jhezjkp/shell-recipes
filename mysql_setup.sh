7fab9b0dd9b1317055d03678d00e19a3b45862f7
jhezjkp <jhezjkp@163.com>
2013-01-30 03:27:31 -0600

    兼容debian

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -11,12 +11,17 @@ else
     generate_password 8
     password=$return_var
     echo -e "\ntime out...generating password...done!"
-    echo $password
 fi
 
 #install
 echo mysql-server mysql-server/root_password password $password | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password $password | sudo debconf-set-selections
-install_package mysql-client-5.5 mysql-server-5.5 libmysqlclient-dev
+if is_ubuntu
+then
+    install_package mysql-client-5.5 mysql-server-5.5 libmysqlclient-dev
+elif is_debian
+then
+    install_package mysql-client libmysqlclient-dev mysql-server
+fi
 echo -en "\n\nThe MySQL root password is:\t"
 print $password
