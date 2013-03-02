64feca8b1ad6abfe7d37f3bb3b0567ed1b7d76f6
jhezjkp <jhezjkp@163.com>
2013-03-02 04:51:09 +0100

    add patch package install

--- basic/scaffold.sh
+++ basic/scaffold.sh
@@ -102,7 +102,7 @@ function replace_repository() {
 function install_basic_packages() {
     log "===== install basic software packages ======"
     install_package zsh make wget curl unzip zip gcc git lrzsz python-setuptools \
-        python-pip vim htop iotop nmon makepasswd bc
+        python-pip vim htop iotop nmon makepasswd bc patch
     log "==== basic software packages installed ====="
 }
 
