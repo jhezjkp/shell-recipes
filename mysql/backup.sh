a7cdd1b47002af4d9d9adfb148aaaafaef925a07
jhezjkp <jhezjkp@163.com>
2015-09-16 20:57:56 +0800

    增加压缩机制

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -13,8 +13,10 @@ databases="db db2"
 
 for db in $databases; do
     printf "backup $db, please wait for a while..."
-    #加上--databases参数后导出的SQL文件中会有建库语句,在把多个数据库备份到一个文件中才会用该参数
+    #加上--databases参数后导出的SQL文件中会有建库语句
     #mysqldump -h$host -P$port -u$user -p$password --databases --default-character-set=utf8 $db > $db.sql
-    mysqldump -h$host -P$port -u$user -p$password --default-character-set=utf8 $db > $db.sql
+    #mysqldump -h$host -P$port -u$user -p$password --default-character-set=utf8 $db > $db.sql
+    #压缩一下
+    mysqldump -h$host -P$port -u$user -p$password --databases --default-character-set=utf8 $db | gzip > $db.sql.gz
     echo "success!!"
 done
